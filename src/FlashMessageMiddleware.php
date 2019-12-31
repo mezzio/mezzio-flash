3dd3bca0f08502324ddaad580f9d609381c798d3
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 09:43:58 -0600

    Rewriting as Laminas Project package

--- src/FlashMessageMiddleware.php
+++ src/FlashMessageMiddleware.php
@@ -1,20 +1,21 @@
 <?php
+
 /**
- * @see       https://github.com/zendframework/zend-expressive-flash for the canonical source repository
- * @copyright Copyright (c) 2017-2018 Zend Technologies USA Inc. (https://www.zend.com)
- * @license   https://github.com/zendframework/zend-expressive-flash/blob/master/LICENSE.md New BSD License
+ * @see       https://github.com/mezzio/mezzio-flash for the canonical source repository
+ * @copyright https://github.com/mezzio/mezzio-flash/blob/master/COPYRIGHT.md
+ * @license   https://github.com/mezzio/mezzio-flash/blob/master/LICENSE.md New BSD License
  */
 
 declare(strict_types=1);
 
-namespace Zend\Expressive\Flash;
+namespace Mezzio\Flash;
 
+use Mezzio\Session\SessionInterface;
+use Mezzio\Session\SessionMiddleware;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Server\RequestHandlerInterface;
-use Zend\Expressive\Session\SessionInterface;
-use Zend\Expressive\Session\SessionMiddleware;
 
 class FlashMessageMiddleware implements MiddlewareInterface
 {
--- test/ConfigProviderTest.php
+++ test/ConfigProviderTest.php
@@ -1,16 +1,17 @@
 <?php
+
 /**
- * @see       https://github.com/zendframework/zend-expressive-flash for the canonical source repository
- * @copyright Copyright (c) 2017 Zend Technologies USA Inc. (https://www.zend.com)
- * @license   https://github.com/zendframework/zend-expressive-flash/blob/master/LICENSE.md New BSD License
+ * @see       https://github.com/mezzio/mezzio-flash for the canonical source repository
+ * @copyright https://github.com/mezzio/mezzio-flash/blob/master/COPYRIGHT.md
+ * @license   https://github.com/mezzio/mezzio-flash/blob/master/LICENSE.md New BSD License
  */
 
 declare(strict_types=1);
 
-namespace ZendTest\Expressive\Flash;
+namespace MezzioTest\Flash;
 
+use Mezzio\Flash\ConfigProvider;
 use PHPUnit\Framework\TestCase;
-use Zend\Expressive\Flash\ConfigProvider;
 
 class ConfigProviderTest extends TestCase
 {
